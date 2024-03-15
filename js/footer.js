43804037965f01fc5ae25d3e57bce3ae574f271f
gasukaku <94212316+gasukaku@users.noreply.github.com>
2024-03-16 00:16:26 +0900

    Create footer.js

---
+++ js/footer.js
@@ -0,0 +1,7 @@
+<footer>
+  <div class="footer">
+  <p>©2021-2024 Gasukaku All copyright.</p>
+  
+  アイコン画像は製作者から著作権を譲渡され、Gasukakuが著作権を有します。</p>
+  <p>※当サイトではGoogleアナリティクスを使用してアクセス分析を行っています。これは個人を特定できるものではありません。
+    </footer>
