02352d8d8196c0ae56c4e0c287503cf6a660ad11
gasukaku <94212316+gasukaku@users.noreply.github.com>
2024-12-31 17:10:14 +0900

    📝リンクページ関連のファイルをアップロード

---
+++ links/script.js
@@ -0,0 +1,6 @@
+  window.addEventListener('load', function() {
+    const loading = document.getElementById('loading');
+    setTimeout(function() {
+      loading.classList.add('hidden');
+    }, 1500);  
+  });
